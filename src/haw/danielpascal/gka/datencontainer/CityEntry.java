c3d2951c2ad06c0639df41368260551fd4e08679
Daniel Müller <lincore@hotmail.de>
2015-10-24 15:08:19 +0200

    Added Entry-Types to CityEntry

--- src/haw/danielpascal/gka/datencontainer/CityEntry.java
+++ src/haw/danielpascal/gka/datencontainer/CityEntry.java
@@ -1,6 +1,6 @@
 package haw.danielpascal.gka.datencontainer;
 
-//Das sollte ein Datencontainer werden der einen Datensatz Stadt vorhält, der dann an die Redis-Klasse zum reinwuppsen geht aus dem Hauptprogramm aus 
+//Das soll ein Datencontainer werden der einen Datensatz Stadt vorhält, der dann an die Redis-Klasse zum reinwuppsen geht aus dem Hauptprogramm aus 
 
 public class CityEntry {
 	
