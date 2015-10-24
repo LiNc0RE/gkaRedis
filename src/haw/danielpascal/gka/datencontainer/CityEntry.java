0e5ab7444f45630a454a77f2977cfa4b2179ae4f
Daniel Müller <lincore@hotmail.de>
2015-10-24 15:05:49 +0200

    Added datatypes to be collected by CityEntry

--- src/haw/danielpascal/gka/datencontainer/CityEntry.java
+++ src/haw/danielpascal/gka/datencontainer/CityEntry.java
@@ -3,7 +3,39 @@ package haw.danielpascal.gka.datencontainer;
 //Das sollte ein Datencontainer werden der einen Datensatz Stadt vorhält, der dann an die Redis-Klasse zum reinwuppsen geht aus dem Hauptprogramm aus 
 
 public class CityEntry {
-
 	
+	private String id;
+	private String city;
+	private double[] loc= new double[2];
+	private int pop;
+	private String state;
+	
+	public CityEntry(String id, String city, double[] loc, int pop, String state){
+		this.id=id;
+		this.city=city;
+		this.loc=loc;
+		this.pop=pop;
+		this.state=state;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public double[] getLoc() {
+		return loc;
+	}
+
+	public int getPop() {
+		return pop;
+	}
+
+	public String getState() {
+		return state;
+	}
 	
 }
